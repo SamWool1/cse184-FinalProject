7e903eeada70c2152693a4b3b4d1eee9465c43ac
SamWooll1 <samwoolledge@gmail.com>
2019-11-23 10:39:28 -0800

    Get num appearances of assignee name

--- wrangle_data.py
+++ wrangle_data.py
@@ -4,12 +4,28 @@
 
 import pandas as pd
 
-def getTopAssignees():
-    pass
+# Takes column of assignee names, returns df with assignee names and how
+# many times they appear (sorted by num of appearances) + percentage
+def getTopAssignees(names):
+
+    # Get number of occurrences for each name
+    count_names = {}
+    for name in names:
+        
+        if name not in count_names:
+            count_names[name] = 1
+        else:
+            count_names[name] = count_names[name] + 1
+    
+    # Get percentage of appearances for each name
+    total_names = len(count_names)
+    for name in count_names:
+        pass
 
 
 def main():
     df = pd.read_csv('scraped_patents.csv')
+    getTopAssignees(df.loc[:, 'Assignee Name'])
     
 
 
