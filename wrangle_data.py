93e4ec1cb054aac45e21a04fd1d41875f71db5db
SamWooll1 <samwoolledge@gmail.com>
2019-11-23 10:28:04 -0800

    Start data wrangling

---
+++ wrangle_data.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------
+# Parse and wrangle data
+# ---------------------------------------------------------------------------
+
+import pandas as pd
+
+def getTopAssignees():
+    pass
+
+
+def main():
+    df = pd.read_csv('scraped_patents.csv')
+    
+
+
+main()
