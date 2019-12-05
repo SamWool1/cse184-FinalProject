994e67ae4dbfdb43e2f36fbc5d1989c2ceefc83b
SamWool1 <56097768+SamWool1@users.noreply.github.com>
2019-12-04 17:04:49 -0800

    Added spreadsheet with every year

--- wrangle_data.py
+++ wrangle_data.py
@@ -6,9 +6,18 @@ import pandas as pd
 import numpy as np
 import datetime
 
+# Creates a single spreadsheet with values for all years
+def createUnifiedScrape():
+    unified_df = pd.DataFrame()
+    for year in range(1980, 2019):
+        year = str(year)
+        df = pd.read_csv('scrapes/scraped_patents' + year + '.csv')
+        df['Year'] = year
+        unified_df = unified_df.append(df)
+    print(unified_df.head())
+    unified_df.to_csv('unified_patents.csv')
+
 # Get a count and percentage of unique values in a column
-
-
 def getCountAndPercent(values):
 
     # Get number of occurrences for each value
@@ -58,8 +67,10 @@ def main(year):
 
     # test_func(df)
 
-for year in range(1980, 2019):
-    print('Wrangling ' + str(year))
-    main(str(year))
-    input('Finished ' + str(year) + ', press Enter to continue...')
-print('All wrangling finished')
+
+createUnifiedScrape()
+# for year in range(1980, 2019):
+#     print('Wrangling ' + str(year))
+#     main(str(year))
+#     input('Finished ' + str(year) + ', press Enter to continue...')
+# print('All wrangling finished')
